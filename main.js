4cc573037ad9d072748c31fc73e226a3f4d48081
Costya Danilov <konstantindanilov19@hotmail.com>
2022-03-29 18:21:55 +0300

    started working with js and learned console, code structure, use strict,variables, data types, alert, prompt, confirm, conditionals(if, else, else if), loops(while, for), switch, object, arrays

---
+++ main.js
@@ -0,0 +1,97 @@
+
+// const todos = [
+//   {
+//     id: 1,
+//     text: 'Take out trash',
+//     isCompleted: true
+//   },
+//   {
+//     id: 2,
+//     text: 'Meeting with boss',
+//     isCompleted: true
+//   },
+//   {
+//     id: 3,
+//     text: 'Dentist appointment',
+//     isCompleted: false
+//   }
+// ];
+
+// // for(let number of todos) {
+// //   console.log(number.id)
+// // }
+// const todoCompleted = todos.filter(function(todo){
+//   return todo.isCompleted === true;
+// }).map(function(todo){
+//   return todo.text;
+// });
+
+
+// console.log(todoCompleted);
+
+
+// Conditionals
+// const x = 10;
+// if(x===10) {
+//   console.log('x is 10');
+// } else if (x > 10) {
+//   console.log('x is greater than 10');
+// }else{
+//   console.log('x is less than 10');
+// }
+
+// const name = prompt('whats your name?');
+// if (name == 'Costya') {
+//   alert('Wow such a coincidence, I m Costya too');
+// }else {
+//   alert('go nd fuck yourself');
+// }
+// const sign = prompt("What's your sign?");
+
+// if (sign.toLowerCase() == "scorpio") {
+//   console.log("Wow! I'm a Scorpio too!");
+// }
+
+// let x = 4;
+// let y = 5;
+// if(x>5){
+//   if(y>10){
+//     console.log('good');
+//   }
+// }
+//    if (x==5){
+//     if(y==10){
+//       console.log('fine')
+//     }
+//   }
+// if(x<5){
+//   if (y<10){
+//     console.log('not good')
+//   }
+// }  
+
+// ternary operator
+
+// const x = 15;
+// const color = x>10 ? 'red' : 'blue';
+// console.log(color)
+
+// let x = 10;
+
+// let color = x>10 ? 'red' : 'blue';
+
+// switch(color) {
+//   case 'red':
+//     console.log('color is red');
+//     break;
+//   case 'blue':
+//     console.log('color is blue');
+//     break;
+//   default:
+//     console.log('color is neither red nor blue');
+//     break;    
+// }
+
+
+
+// functions
