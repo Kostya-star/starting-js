58a14995d0380fd11b91325e872b146fafe6ab72
Costya Danilov <konstantindanilov19@hotmail.com>
2022-03-31 16:19:23 +0300

    finished learning the the basic tools about js and started to solidify my knowledge on it(going in depth)

--- main.js
+++ main.js
@@ -329,9 +329,38 @@
 // const btn = document.querySelector('.btn');
 // btn.style.background = 'red';
 
-const btn = document.querySelector('.btn');
+// const btn = document.querySelector('.btn');
 
-btn.addEventListener('click', (e) => {
-  e.preventDefault();
-  console.log('click');
-});
+// btn.addEventListener('mouseout', (e) => {
+//   e.preventDefault();
+//   document.querySelector('#my-form').style.background = '#439004';
+//   document.querySelector('body').classList.add('bg-dark');
+//   document.querySelector('.items').lastElementChild.innerHTML = '<h1>Hello!</h1>'
+// });
+
+// const myForm = document.querySelector('#my-form');
+// const nameInput = document.querySelector('#name');
+// const emailInput = document.querySelector('#email');
+// const msg = document.querySelector('.msg');
+// const userList = document.querySelector('#users');
+
+// myForm.addEventListener('submit', onSubmit);
+
+// function onSubmit(e) {
+//   e.preventDefault();
+
+//   if(nameInput.value === '' || emailInput.value === '') {
+//     msg.classList.add('error');
+//     msg.innerHTML = 'plz enter all the fiels!';
+
+//     setTimeout(() => msg.remove(), 3000);
+//   } else {
+//     const li = document.createElement('li');
+//     li.appendChild(document.createTextNode(`${nameInput.value} : ${emailInput.value}`));
+
+//     userList.appendChild(li);
+
+//     nameInput.value = '';
+//     emailInput = '';
+//   }
+// }
